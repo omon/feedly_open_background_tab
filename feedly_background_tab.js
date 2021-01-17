8598e79195ebd53988755ffd8f19d9fb05290bf6
omon <otfmcmrf2@gmail.com>
2021-01-17 16:13:30 +0900

    fix selector for to work tile only view , article view , magazine view and card view.

--- feedly_background_tab.js
+++ feedly_background_tab.js
@@ -1,20 +1,29 @@
 (function(){
-    var selectors = [
+	// for article mode.
+	var article_mode_selector = ".inlineFrame--selected";
+	var entry_title_selector = ".entryTitle";
+	var floating_mode_selector = ".floatingEntryContent";
+	
+	var selectors = [
+		article_mode_selector,				
 		'div.selectedEntry a.title',			
 		'.selectedEntry a.visitWebsiteButton',	
 		'.list-entries .selected a.visitWebsiteButton',
-		'a.visitWebsiteButton',					
-		'.entry.selected a.title'			
-    ]
-	
+		'a.visitWebsiteButton',
+		'.entry.selected a.title',				
+		'.entry--selected a.entry__title'	 // add feedback		
+	]
+
 	var App = function() {
-		var _triggerCharCode = 104
+		var _triggerCharCode = 104  // default is 'h'
 
 		this.init = function() {
+			// if setting another shortcut key,,, change trigger
 			chrome.storage.local.get("shortcut", items=>{
 				if (typeof items.shortcut === "undefined") {
 				}else{
 					_triggerCharCode = items.shortcut.charCodeAt(0)
+					console.log("change key by setting")
 				}
 			})
 		}
@@ -24,10 +33,24 @@
 			if (tag != 'input' && tag != 'textarea') {
 				if (e.charCode == _triggerCharCode) {
 					var url
-                    for (var x in selectors) {
-                        url = document.querySelector(selectors[x])
-                        if(url){break}
-                    }
+                    for (var x in selectors) {			// for Article mode
+						url = document.querySelector(selectors[x])
+						if(selectors[x] == article_mode_selector){
+							// console.log("catch selectors from: " + selectors[x])
+							var isFloting = document.querySelector(floating_mode_selector)
+							if(isFloting){
+								// console.log("floating mode")
+								url = isFloting.querySelector(entry_title_selector)
+							}else{
+								// console.log("article mode")
+								url = url.querySelector(entry_title_selector)
+							}
+							// console.log("url is " + url)
+						}else{
+							continue;
+						}
+						if(url){break}
+					}
 					if (url) {
 						chrome.runtime.sendMessage({url: url.href})
 					}
