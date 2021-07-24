f75d85a341ba65429a6ecf64473b967a226a5313
junichi_ohashi <otfmcmrf2@gmail.com>
2021-07-24 12:52:51 +0900

    Fixed deprecated keypress event to keydown event.

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 firefox addon for feedly
 
 
+Ver3.0
+ Fixed deprecated keypress event to keydown event. 
+ 
 Ver2.0
  fix selector for to work tile only view , article view , magazine view and card view.
--- feedly_background_tab.js
+++ feedly_background_tab.js
@@ -1,62 +1,62 @@
-(function(){
+(function () {
 	// for article mode.
 	var article_mode_selector = ".inlineFrame--selected";
 	var entry_title_selector = ".entryTitle";
 	var floating_mode_selector = ".floatingEntryContent";
-	
+
 	var selectors = [
-		article_mode_selector,				
-		'div.selectedEntry a.title',			
-		'.selectedEntry a.visitWebsiteButton',	
+		article_mode_selector,
+		'div.selectedEntry a.title',
+		'.selectedEntry a.visitWebsiteButton',
 		'.list-entries .selected a.visitWebsiteButton',
 		'a.visitWebsiteButton',
-		'.entry.selected a.title',				
+		'.entry.selected a.title',
 		'.entry--selected a.entry__title'	 // add feedback		
 	]
 
-	var App = function() {
-		var _triggerCharCode = 104  // default is 'h'
-
-		this.init = function() {
+	var App = function () {
+		var _triggerCharCode = "KeyH"  // default is 'h'
+		this.init = function () {
 			// if setting another shortcut key,,, change trigger
-			chrome.storage.local.get("shortcut", items=>{
+			chrome.storage.local.get("shortcut", items => {
 				if (typeof items.shortcut === "undefined") {
-				}else{
-					_triggerCharCode = items.shortcut.charCodeAt(0)
-					console.log("change key by setting")
+				} else {
+					_triggerCharCode = "Key" + items.shortcut.toUpperCase();
+					// console.log("change key by setting")
+					// console.log(_triggerCharCode)
 				}
 			})
 		}
 
-		this.keyPressHandler = function(e) {
+		this.keyDownHandler = function (e) {
 			var tag = e.target.tagName.toLowerCase()
 			if (tag != 'input' && tag != 'textarea') {
-				if (e.charCode == _triggerCharCode) {
+				if (e.code == _triggerCharCode) {
 					var url
-                    for (var x in selectors) {			// for Article mode
+					for (var x in selectors) {			// for Article mode
 						url = document.querySelector(selectors[x])
-						if(selectors[x] == article_mode_selector){
+						if (selectors[x] == article_mode_selector) {
 							// console.log("catch selectors from: " + selectors[x])
 							var isFloting = document.querySelector(floating_mode_selector)
-							if(isFloting){
+							if (isFloting) {
 								// console.log("floating mode")
 								url = isFloting.querySelector(entry_title_selector)
-							}else{
+							} else {
 								// console.log("article mode")
 								url = url.querySelector(entry_title_selector)
 							}
 							// console.log("url is " + url)
-						}else{
+						} else {
 							continue;
 						}
-						if(url){break}
+						if (url) { break }
 					}
 					if (url) {
-						chrome.runtime.sendMessage({url: url.href})
+						chrome.runtime.sendMessage({ url: url.href })
 					}
-                    else {
-                        console.log("Could not find any selectors from: " + selectors.join())
-                    }
+					else {
+						console.log("Could not find any selectors from: " + selectors.join())
+					}
 				}
 			}
 		}
@@ -65,7 +65,7 @@
 	if (window == top) {
 		var app = new App()
 		app.init()
-		window.addEventListener('keypress', app.keyPressHandler, false)
+		window.addEventListener('keydown', app.keyDownHandler, false)
 
 	}
 })()
